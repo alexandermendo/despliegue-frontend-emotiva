49a733bed01a5966388e611507277e44fd6e94a0
Alexander Mendoza <alexamp1987@gmail.com>
2024-05-10 12:53:52 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Cambia '/my-react-project/' a tu ruta base deseada
-  plugins: [
-    react({
-      jsx: 'react-jsx',
-    }),
-  ],
+  plugins: [react()],
 })
